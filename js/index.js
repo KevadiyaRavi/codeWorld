41cc7119deb13e8997650fdce7c497b2624d4085
Ravi <kevadiyaravi87@gmail.com>
2022-06-19 08:06:37 +0530

    done

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 
-
 var list = document.querySelectorAll('.list li a');
 list.forEach(function (item) {
+
     item.addEventListener('click', (e) => {
-        localStorage.setItem('urlName', e.target.classList)
+        console.log(e.currentTarget)
+        localStorage.setItem('urlName', e.currentTarget.classList)
+
     })
 })
+
+
 
 
