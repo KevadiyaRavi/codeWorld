33272e91e59650646b5e1cbe8cbf07b72bf041b0
Ravi <kevadiyaravi87@gmail.com>
2022-06-20 21:02:18 +0530

    done

--- test.c
+++ test.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+void print_array(int *p, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", p[i]);
+    }
+}
 int main()
 {
-    int array[] = {3, 4, 1, 2, 3, 7, 6, 5};
-    int size = sizeof(array) / sizeof(array[0]);
-    printf("Actual array: ");
-    for (int i = 0; i < size; printf("%d ", array[i++]))
-        ;
-    printf("\nPrinting array in reverse order: ");
-    for (int i = size - 1; i >= 0; printf("%d ", array[i--]))
-        ;
-    for (int i = 0; i <= size / 2; i++)
-    {
-        int t = array[i];
-        array[i] = array[size - 1 - i];
-        array[size - 1 - i] = t;
-    }
-    printf("\nAfter reversing array itself: ");
-    for (int i = 0; i < size; printf("%d ", array[i++]))
-        ;
+    int array[] = {10, 20, 30, 40};
+    print_array(array, sizeof(array) / sizeof(array[0]));
 }
